9b0d3b6f12dc631556ebc3266499489598f6ab9b
CppXL <shijieanhaocqy@163.com>
2022-03-03 17:19:05 +0800

    update config

--- config/config.go
+++ config/config.go
@@ -112,6 +112,7 @@ func checkConfig() error {
 	if value, err := utils.IsNumInRange(SConfig.Server.ControlPort, MinPort, MaxPort, [2]byte{'(', ')'}); !value && err == nil {
 		return errors.New("control port out of range")
 	}
+
 	// 是否相等 端口
 	if SConfig.Server.ListenPort == SConfig.Server.ControlPort {
 		return errors.New("listen port and control port is equal")
